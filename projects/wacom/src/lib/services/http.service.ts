4d1b326440e4103a5cfd67807469122255bc339d
Arthur <hlushko.arthur@gmail.com>
2023-07-19 19:51:53 +0300

    console.log remove

--- projects/wacom/src/lib/services/http.service.ts
+++ projects/wacom/src/lib/services/http.service.ts
@@ -62,8 +62,6 @@ export class HttpService {
 		});
 
 		this.store.getJson('http_headers', (headers: any) => {
-			console.log('headers', headers);
-			
 			if (headers) {
 				for (const header in headers) {
 					this._headers[header] = headers[header];
