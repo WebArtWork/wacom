4c965f4437b3e00033f1c93792a568519d97b672
Honchar Denys <crackeraki@gmail.com>
2023-11-24 17:17:33 +0200

    init alert refactorer

--- projects/wacom/src/lib/services/alert.service.ts
+++ projects/wacom/src/lib/services/alert.service.ts
@@ -7,97 +7,121 @@ import { CoreService } from './core.service';
 import { Alert, DEFAULT_Alert } from '../interfaces/alert.interface';
 
 @Injectable({
-	providedIn: 'root'
+	providedIn: 'root',
 })
 export class AlertService {
-	private alert:any;
-	constructor(private dom: DomService, private core: CoreService,
-		@Inject(CONFIG_TOKEN) @Optional() private config: Config) {
-		if(!this.config) this.config = DEFAULT_CONFIG;
+	private alert: any;
+	constructor(
+		private dom: DomService,
+		private core: CoreService,
+		@Inject(CONFIG_TOKEN) @Optional() private config: Config
+	) {
+		if (!this.config) this.config = DEFAULT_CONFIG;
 		this.alert = this.config.alert;
-		if(!this.alert) {
+		if (!this.alert) {
 			this.alert = DEFAULT_Alert;
 		} else {
-			for (let each in DEFAULT_Alert){
-				if(this.alert[each]) continue;
-			    this.alert[each] = DEFAULT_Alert[each];
+			for (let each in DEFAULT_Alert) {
+				if (this.alert[each]) continue;
+				this.alert[each] = DEFAULT_Alert[each];
 			}
 		}
 		this.dom.appendComponent(WrapperComponent);
 	}
-	private uniques:any = {};
-	private shortcuts:any = {
-		tl: "topLeft",
-		tc: "topCenter",
-		tr: "topRight",
-		r: "right",
-		br: "bottomRight",
-		bc: "bottomCenter",
-		bl: "bottomLeft",
-		l: "left",
-		c: "center"
+	private uniques: any = {};
+	private shortcuts: any = {
+		tl: 'topLeft',
+		tc: 'topCenter',
+		tr: 'topRight',
+		r: 'right',
+		br: 'bottomRight',
+		bc: 'bottomCenter',
+		bl: 'bottomLeft',
+		l: 'left',
+		c: 'center',
 	};
-	show(opts: any | Alert){
+	show(opts: any | Alert) {
 		if (typeof opts === 'string') {
 			opts = {
-				text: opts
-			}
+				text: opts,
+			};
 		}
 		if (!opts) opts = {};
 		if (!opts['type']) opts['type'] = 'info';
-		for (let each in this.alert){
-			if(each=="class") opts[each] = opts[each]+" "+this.alert[each];
-			else if(typeof opts[each] == "undefined") opts[each] = this.alert[each];
+		for (let each in this.alert) {
+			if (each == 'class') opts[each] = opts[each] + ' ' + this.alert[each];
+			else if (typeof opts[each] == 'undefined') opts[each] = this.alert[each];
 		}
-		if(this.shortcuts[opts.position]) opts.position = this.shortcuts[opts.position];
-		if(!opts.position) opts.position='bottomRight';
-		var content:any;
-		opts.close = ()=>{
-			if(content) content.componentRef.destroy();
+		if (this.shortcuts[opts.position])
+			opts.position = this.shortcuts[opts.position];
+		if (!opts.position) opts.position = 'bottomRight';
+		var content: any;
+		opts.close = () => {
+			if (content) content.componentRef.destroy();
 			component.nativeElement.remove();
-			if (typeof (opts as Alert).onClose == 'function') (opts as Alert).onClose();
+			if (typeof (opts as Alert).onClose == 'function')
+				(opts as Alert).onClose();
 		};
 		let component = this.dom.appendById(AlertComponent, opts, opts.position);
-		if(typeof opts.component == 'string' && this.alert.alerts[opts.component]){
+		if (
+			typeof opts.component == 'string' &&
+			this.alert.alerts[opts.component]
+		) {
 			opts.component = this.alert.alerts[opts.component];
 		}
-		if(typeof opts.component == 'function'){
-			content = this.dom.appendComponent(opts.component, opts, component.nativeElement.children[0].children[0].children[0] as HTMLElement);
+
+		console.log(component.nativeElement);
+		if (
+			typeof opts.component === 'function' &&
+			component.nativeElement &&
+			Array.isArray(component.nativeElement.children) &&
+			Array.isArray(component.nativeElement.children[0].children) &&
+			Array.isArray(component.nativeElement.children[0].children[0].children)
+		) {
+			content = this.dom.appendComponent(
+				opts.component,
+				opts,
+				component.nativeElement.children[0].children[0]
+					.children[0] as HTMLElement
+			);
 		}
-		if(opts.unique){
-			if(this.uniques[opts.unique]) this.uniques[opts.unique].remove();
+
+		if (opts.unique) {
+			if (this.uniques[opts.unique]) this.uniques[opts.unique].remove();
 			this.uniques[opts.unique] = component.nativeElement;
 		}
 		return component.nativeElement;
 	}
-	open(opts: Alert){ this.show(opts); }
-	info(opts: Alert){
-		opts['type']='info';
+	open(opts: Alert) {
 		this.show(opts);
 	}
-	success(opts: Alert){
-		opts['type']='success';
+	info(opts: Alert) {
+		opts['type'] = 'info';
 		this.show(opts);
 	}
-	warning(opts: Alert){
-		opts['type']='warning';
+	success(opts: Alert) {
+		opts['type'] = 'success';
 		this.show(opts);
 	}
-	error(opts: Alert){
-		opts['type']='error';
+	warning(opts: Alert) {
+		opts['type'] = 'warning';
 		this.show(opts);
 	}
-	question(opts: Alert){
-		opts['type']='question';
+	error(opts: Alert) {
+		opts['type'] = 'error';
 		this.show(opts);
 	}
-	destroy(){
-		this.core.document.getElementById("bottomRight").innerHTML = "";
-		this.core.document.getElementById("bottomLeft").innerHTML = "";
-		this.core.document.getElementById("bottomCenter").innerHTML = "";
-		this.core.document.getElementById("topRight").innerHTML = "";
-		this.core.document.getElementById("topLeft").innerHTML = "";
-		this.core.document.getElementById("topCenter").innerHTML = "";
-		this.core.document.getElementById("center").innerHTML = "";
+	question(opts: Alert) {
+		opts['type'] = 'question';
+		this.show(opts);
+	}
+	destroy() {
+		this.core.document.getElementById('bottomRight').innerHTML = '';
+		this.core.document.getElementById('bottomLeft').innerHTML = '';
+		this.core.document.getElementById('bottomCenter').innerHTML = '';
+		this.core.document.getElementById('topRight').innerHTML = '';
+		this.core.document.getElementById('topLeft').innerHTML = '';
+		this.core.document.getElementById('topCenter').innerHTML = '';
+		this.core.document.getElementById('center').innerHTML = '';
 	}
 }
