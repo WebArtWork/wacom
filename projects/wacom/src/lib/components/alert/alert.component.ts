1e4ab2fbe5e2b14ab3d6975b7c2ebf2708eeee27
Honchar Denys <crackeraki@gmail.com>
2024-05-24 17:56:21 +0300

    prepare upgrade

--- projects/wacom/src/lib/components/alert/alert.component.ts
+++ projects/wacom/src/lib/components/alert/alert.component.ts
@@ -1,18 +1,17 @@
-import { Component, ElementRef, ViewChild} from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { CoreService } from '../../services/core.service';
 
 @Component({
 	selector: 'alert',
 	templateUrl: './alert.component.html',
-	styleUrls: ['./alert.component.scss']
+	styleUrls: ['./alert.component.scss'],
 })
-
 export class AlertComponent {
-	@ViewChild('alert', {static:false}) alert:any;
+	@ViewChild('alert', { static: false }) alert: any;
 	component: any;
-	text: string = "";
-	class: string = "";
-	type: string = "info";
+	text: string = '';
+	class: string = '';
+	type: string = 'info';
 	progress: boolean = true;
 	position: string = 'bottomRight'; // [bottomRight, bottomLeft, topRight, topLeft, topCenter, bottomCenter or center]
 	icon: string = '';
@@ -21,31 +20,42 @@ export class AlertComponent {
 	closable: any = true;
 	buttons: any = []; /*[{text, callback}]*/
 
-	constructor(private elementRef: ElementRef, public core: CoreService) {
-		setTimeout(()=>{
-			if(this.timeout){
-				let remaining=JSON.parse(JSON.stringify(this.timeout));
-				let timer=setTimeout(()=>{this.remove()}, remaining);
+	constructor(public core: CoreService) {
+		setTimeout(() => {
+			if (this.timeout) {
+				let remaining = JSON.parse(JSON.stringify(this.timeout));
+				let timer = setTimeout(() => {
+					this.remove();
+				}, remaining);
 				let start = new Date();
-				this.alert.nativeElement.addEventListener("mouseenter", ()=>{
-					clearTimeout(timer);
-					remaining -= new Date().getTime() - start.getTime();
-				}, false);
-				this.alert.nativeElement.addEventListener("mouseleave", ()=>{
-					start = new Date();
-					clearTimeout(timer);
-					timer = core.window.setTimeout(()=>{this.remove()}, remaining);
-				}, false);
+				this.alert.nativeElement.addEventListener(
+					'mouseenter',
+					() => {
+						clearTimeout(timer);
+						remaining -= new Date().getTime() - start.getTime();
+					},
+					false
+				);
+				this.alert.nativeElement.addEventListener(
+					'mouseleave',
+					() => {
+						start = new Date();
+						clearTimeout(timer);
+						timer = core.window.setTimeout(() => {
+							this.remove();
+						}, remaining);
+					},
+					false
+				);
 			}
 		});
 	}
-	public delete_animation=false;
-	remove(){
-		this.delete_animation=true;
-		setTimeout(()=>{
+	public delete_animation = false;
+	remove() {
+		this.delete_animation = true;
+		setTimeout(() => {
 			this.close();
-			this.delete_animation=false;
-		},350);
-
+			this.delete_animation = false;
+		}, 350);
 	}
 }
