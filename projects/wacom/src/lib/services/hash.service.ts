a3d6de53bb40fede32350d76bb8e916cd92dc96f
Honchar Denys <crackeraki@gmail.com>
2024-05-29 13:44:57 +0300

    fixed hash issue with new hash strategy

--- projects/wacom/src/lib/services/hash.service.ts
+++ projects/wacom/src/lib/services/hash.service.ts
@@ -2,59 +2,69 @@
 import { Injectable } from '@angular/core';
 import { CoreService } from './core.service';
 @Injectable({
-	providedIn: 'root'
+	providedIn: 'root',
 })
 export class HashService {
-	private replaces = [{
-		from: '%20',
-		to: ' '
-	}];
-	public hash:any = {};
+	private replaces = [
+		{
+			from: '%20',
+			to: ' ',
+		},
+	];
+	public hash: any = {};
 	private done: boolean = false;
 	constructor(public core: CoreService) {
-		if(!this.core.window.location.hash){
+		if (!this.core.window.location.hash) {
 			this.done = true;
 			return;
 		}
 		this.load();
 		this.done = true;
 	}
-	load(){
+	load() {
 		this.hash = {};
-		let hash:any = this.core.window.location.hash.replace('#!#', '').replace('#', '').split('&');
-		for(let i = 0; i < hash.length; i++){
+		let hash: any = this.core.window.location.hash
+			.replace('#!#', '')
+			.replace('#', '')
+			.split('&');
+		for (let i = 0; i < hash.length; i++) {
 			let holder = hash[i].split('=')[0];
 			let value = hash[i].split('=')[1];
-			for(let j = 0; j < this.replaces.length; j++){
-				holder = holder.split(this.replaces[j].from).join(this.replaces[j].to);
-				value = value.split(this.replaces[j].from).join(this.replaces[j].to);
+			for (let j = 0; j < this.replaces.length; j++) {
+				holder = (holder || '')
+					.split(this.replaces[j].from)
+					.join(this.replaces[j].to);
+				value = (value || '')
+					.split(this.replaces[j].from)
+					.join(this.replaces[j].to);
 			}
 			this.hash[holder] = value;
 		}
 	}
-	on(field:any, cb = (resp:any)=>{}):any{
-		if(!this.done) return setTimeout(()=>{
-			this.on(field, cb);
-		}, 100);
+	on(field: any, cb = (resp: any) => {}): any {
+		if (!this.done)
+			return setTimeout(() => {
+				this.on(field, cb);
+			}, 100);
 		cb(this.hash[field]);
 	}
-	save(){
+	save() {
 		let hash = '';
-		for(let each in this.hash){
-			if(hash) hash += '&';
+		for (let each in this.hash) {
+			if (hash) hash += '&';
 			hash += each + '=' + this.hash[each];
 		}
 		this.core.window.location.hash = hash;
 	}
-	set(field:any, value:any){
+	set(field: any, value: any) {
 		this.hash[field] = value;
 		this.save();
 	}
-	get(field:any){
+	get(field: any) {
 		return this.hash[field];
 	}
-	clear(field?:any){
-		if(field) delete this.hash[field];
+	clear(field?: any) {
+		if (field) delete this.hash[field];
 		else this.hash = {};
 		this.save();
 	}
