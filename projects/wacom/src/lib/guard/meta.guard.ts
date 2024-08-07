b88a01f9a36dbaf61c35196097ac6ab8d8c62457
Narolskyi Denis <asotizianov@gmail.com>
2024-08-07 11:45:39 +0300

    fixed meta management

--- projects/wacom/src/lib/guard/meta.guard.ts
+++ projects/wacom/src/lib/guard/meta.guard.ts
@@ -4,15 +4,20 @@ import { MetaService } from '../services/meta.service';
 import { CONFIG_TOKEN, Config, DEFAULT_CONFIG } from '../interfaces/config';
 
 @Injectable()
-export class MetaGuard  {
+export class MetaGuard {
 	public static IDENTIFIER = 'MetaGuard';
-	private _meta: any ;
-	public constructor(private metaService: MetaService,
-		@Inject(CONFIG_TOKEN) @Optional() private config: Config) {
+	private _meta: any;
+	public constructor(
+		private metaService: MetaService,
+		@Inject(CONFIG_TOKEN) @Optional() private config: Config
+	) {
 		this._meta = config.meta;
-		if(!this.config) this.config = DEFAULT_CONFIG;
+		if (!this.config) this.config = DEFAULT_CONFIG;
 	}
-	public canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+	public canActivate(
+		route: ActivatedRouteSnapshot,
+		state: RouterStateSnapshot
+	): boolean {
 		this._processRouteMetaTags(route.data && route.data['meta']);
 		return true;
 	}
@@ -20,18 +25,38 @@ export class MetaGuard  {
 		if (meta.disableUpdate) {
 			return;
 		}
-		this.metaService.setTitle(meta.title, meta.titleSuffix);
-		this.metaService.setLink(meta.link);
-		Object.keys(meta).forEach(prop => {
-			if (prop === 'title' || prop === 'titleSuffix'|| prop === 'link') {
+		if (meta.title) {
+			this.metaService.setTitle(meta.title, meta.titleSuffix);
+		}
+		if (Array.isArray(meta.links)) {
+			this.metaService.setLink(meta.links);
+		} else if (typeof meta.links === 'string') {
+			this.metaService.setLink(meta.links.split(' '));
+		}
+		if (Array.isArray(this._meta.defaults?.links)) {
+			this.metaService.setLink(this._meta.defaults?.links);
+		} else if (typeof this._meta.defaults?.links === 'string') {
+			this.metaService.setLink(this._meta.defaults?.links.split(' '));
+		}
+		Object.keys(meta).forEach((prop) => {
+			if (
+				prop === 'title' ||
+				prop === 'titleSuffix' ||
+				prop === 'links'
+			) {
 				return;
 			}
-			Object.keys(meta[prop]).forEach(key => {
+			Object.keys(meta[prop]).forEach((key) => {
 				this.metaService.setTag(key, meta[prop][key], prop);
 			});
 		});
-		Object.keys(this._meta.defaults).forEach(key => {
-			if (key in meta || key === 'title' || key === 'titleSuffix' || key === 'link') {
+		Object.keys(this._meta.defaults).forEach((key) => {
+			if (
+				key in meta ||
+				key === 'title' ||
+				key === 'titleSuffix' ||
+				key === 'links'
+			) {
 				return;
 			}
 			this.metaService.setTag(key, this._meta.defaults[key]);
