5078a9b03b09f84201a1c0b4072c8ee1e6461ad7
Honchar Denys <crackeraki@gmail.com>
2025-08-14 23:49:46 +0300

    cleanup src

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,42 +1,9 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { StoreService, ModalService, UiService, AlertService } from 'wacom';
-import { LocalComponent } from './modals/local/local.component';
-import { CoreComponent } from './doc/service/core/core.component';
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss'],
-    standalone: true,
-    imports: [CommonModule, FormsModule, CoreComponent]
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	imports: [CommonModule, FormsModule],
 })
-export class AppComponent {
-	public password = '';
-	local_modal(){
-		this.modal.show({
-			component: LocalComponent
-		});
-	}
-	constructor(
-		public store: StoreService,
-		public modal: ModalService,
-		public ui: UiService,
-		private _alert: AlertService
-	) {
-		this._alert.show({
-			component: 'test',
-			timeout: 0
-		});
-
-		// this._alert.show({
-		// 	text: 'Hello World',
-		// 	timeout: 0
-		// });
-
-		this.store.get('test', (message:any)=>{
-			console.log('GET: ', message);
-		});
-		this.store.set('test', 'Hello World');
-	}
-}
+export class AppComponent {}
--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,9 @@
-import { bootstrapApplication } from '@angular/platform-browser';
 import { importProvidersFrom } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { WacomModule } from 'wacom';
+import { bootstrapApplication, BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
-import { TestalertComponent } from './app/testalert/testalert.component';
 
 bootstrapApplication(AppComponent, {
-  providers: [
-    importProvidersFrom(
-      BrowserModule,
-      FormsModule,
-      WacomModule.forRoot({
-        socket: false,
-        alert: {
-          alerts: { test: TestalertComponent }
-        }
-      })
-    )
-  ]
-}).catch(err => console.error(err));
-
+	providers: [importProvidersFrom(BrowserModule, FormsModule)],
+}).catch((err) => console.error(err));
