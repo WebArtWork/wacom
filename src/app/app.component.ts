2591908bff69f196540802d54b5486869f4aa2e4
Nika <danzi6386@gmail.com>
2025-08-21 16:38:00 +0300

    added loaders configs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { Router, RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
-	imports: [CommonModule, FormsModule, RouterOutlet, RouterLink],
+	imports: [CommonModule, RouterOutlet, RouterLink],
 })
 export class AppComponent {
 	router = inject(Router);
