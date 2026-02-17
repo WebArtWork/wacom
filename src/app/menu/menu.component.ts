81217632527360b7c99ddfb3bf44fbe5d2cac391
Honchar Denys <crackeraki@gmail.com>
2026-02-17 09:06:23 +0200

    cleanup

--- src/app/menu/menu.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
-
-@Component({
-	imports: [RouterLink],
-	templateUrl: './menu.component.html',
-})
-export class MenuComponent {}
--- src/app/modals/small/small.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-	selector: 'app-small',
-	imports: [],
-	templateUrl: './small.component.html',
-})
-export class SmallComponent {}
